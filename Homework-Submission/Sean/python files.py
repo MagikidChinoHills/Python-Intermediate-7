7608e14c7c79f7c63ab32fefd84d2d89faf13d8d
Sean Wu <swu516@outlook.com>
2025-04-11 19:17:24 -0700

    Add files via upload
    I could only do steps 1 2 and 3.
    =(

---
+++ Homework-Submission/Sean/python files.py
@@ -0,0 +1,14 @@
+age=9
+print(f"My age is {age}")
+name="Sean Wu"
+print(f"My name is {name}")
+temp=76.00
+a=True
+b=False
+result=a or b
+print("Is it sunny:",result)
+print("the temperature is",temp)
+#temp is a float
+#a and b is boolean >=(
+#age is integer
+#name is a string
