8308103d8fa8051b503f1284420e9fbc7fd93398
Sean Wu <swu516@outlook.com>
2025-04-19 17:24:50 -0700

    Add files via upload

---
+++ Homework-Submission/Sean/python_homework_week3.py
@@ -0,0 +1,54 @@
+num1=93
+num2=2
+addition="The sum of 93 and 2 is "+str(num1+num2)
+print(addition)
+subtraction="The difference of 93 and 2 is "+str(num1-num2)
+print(subtraction)
+multiplication="The product of 93 and 2 is "+str(num1*num2)
+print(multiplication)
+division="The difference of 93 and 2 is "+str(num1/num2)
+print(division)
+modulus="The remainder of 93 divided by 2 is "+str(num1%num2)
+print(modulus)
+if num1 == num2:
+    print("Is 93 equal to 2? true")
+else:
+    print("Is 93 equal to 2? false")
+if num1 != num2:
+    print("Is 93 not equal to 2? true")
+else:
+    print("Is 93 not equal to 2? false")
+if num1 > num2:
+    print("Is 93 greater than 2? true")
+else:
+    print("Is 93 greater than 2? false")
+if num1 < num2:
+    print("Is 93 less than 2? true")
+else:
+    print("Is 93 less than 2? false")
+if num1 >= num2:
+    print("Is 93 greater than or equal to 2? true")
+else:
+    print("Is 93 greater than or equal to 2? false")
+if num1 <= num2:
+    print("Is 93 less than or equal to 2? true")
+else:
+    print("Is 93 less than or equal to 2? false")
+    # Check if num1 is greater than 0 and num2 is less than 10
+    # Check if num1 is greater than 0 and num2 is less than 10
+if num1 > 0 and num2 < 10:
+    print(f"Is {num1} greater than 0 and {num2} less than 10? True")
+else:
+    print(f"Is {num1} greater than 0 and {num2} less than 10? False")
+
+# Check if num1 is equal to 5 or num2 is equal to 8
+if num1 == 5 or num2 == 8:
+    print(f"Is {num1} equal to 5 or {num2} equal to 8? True")
+else:
+    print(f"Is {num1} equal to 5 or {num2} equal to 8? False")
+
+# Check if num1 is not equal to num2
+if num1 != num2:  # This checks if num1 is not equal to num2
+    print("Is 93 not equal to 2? True")
+else:
+     print("Is 93 not equal to 2? False")
