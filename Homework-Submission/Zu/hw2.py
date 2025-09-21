25c3fa6042c99da676aa8d41b0412e4d7240351d
Zuu244 <zulekaer127127@gmail.com>
2025-09-21 12:53:58 -0700

    Update hw2.py

--- Homework-Submission/Zu/hw2.py
+++ Homework-Submission/Zu/hw2.py
@@ -1,11 +1,11 @@
 #1
-import turtle
+import turtle 
 import random
 
 t = turtle.Turtle()
 t.speed(0)
 
-for _ in range(100):  
+for _ in range(50):  
     distance = random.randint(10, 50)   
     angle = random.randint(0, 180)      
     t.forward(distance)
@@ -26,6 +26,7 @@ t.speed(0)
 colors = ["red", "blue", "green", "yellow", "purple", "orange"]
 
 for _ in range(100):
+    t.pencolor(random.choice(colors))
     distance = random.randint(10, 50)
     angle = random.randint(0, 180)
     t.color(random.choice(colors))   
@@ -77,7 +78,21 @@ for i in range(3):
     t.pendown()
 
 turtle.done()
+///
+import turtle 
+    def spriograph(angle,x,y,color):
+        t.penup()
+        t.goto(x,y)
+        t.pendown()
+        t.pencolor(color)
+        for i in range(int(360.angle)):
+            t.circle(100)
+            t.right(angle)
+spirograph(5,-200,0,"red")
+spirograph(10,0,0,"blue")
+spirograph(15,200,0,"green")
 
+t.done()
 
 
 #5
@@ -98,3 +113,4 @@ def random_spirograph(angle):
 
 random_spirograph(10)
 turtle.done()
+
