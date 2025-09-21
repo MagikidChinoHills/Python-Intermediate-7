738d4ef1f445fdb4d1e75958a9c5a2e48b93d3ca
Zuu244 <zulekaer127127@gmail.com>
2025-09-21 13:05:45 -0700

    Update inthw3.py

--- Homework-Submission/Zu/inthw3.py
+++ Homework-Submission/Zu/inthw3.py
@@ -1,7 +1,27 @@
-#1
-length = 5 
+import turtle
+
+t = turtle.Turtle()
+t.speed(0)
+length = 1
 for i in range (50):
   t.forward(length)
-  t.right(20)
-  length +=5
+  t.right(30)
+  length +=1
+t.done()
+
+
+
+
+#2
+
+
+def draw_diamond(size):
+  for i in range(2):
+    t.forward(size)
+    t.left(60)
+    t.forward(size)
+    t.left(120)
+for i in range (12):
+  draw_diamond(100)
+  t.left(30)
 t.done()
